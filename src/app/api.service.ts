0624608b57b83c1baf6ae2b0dc6913a883fd7a95
bleerobs <bleerobs@gmail.com>
2019-08-24 17:26:45 -0400

    working angular api demo

---
+++ src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(private httpClient: HttpClient) {}
+
+  public getNews() {
+    return this.httpClient.get(
+      `https://localhost:44316/api/values`
+    );
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { NewsComponent } from './news/news.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
